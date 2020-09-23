e5fe0bdecbeb479cc8d8108325a5f50ac34ce864
mixnr1 <miks.grinbergs@gmail.com>
2020-09-23 10:01:39 +0300

    Added more fields to csv

--- script.py
+++ script.py
@@ -2,33 +2,52 @@ import os
 import re
 import time
 from selenium import webdriver
-import requests
+from bs4 import BeautifulSoup
 import config
 start = time.time()
+
 path=config.path
 driver = webdriver.Firefox(executable_path=config.driver_path)
 my_list=open(path+'list.txt', 'r')
 result_file=open(path+"result.csv",'a')
-for elem in my_list:
-    driver.get('https://info.ur.gov.lv/?#/legal-entity/'+elem)
-    time.sleep(3)
-    Url=str('https://info.ur.gov.lv/?#/legal-entity/'+elem)
-    Nosaukums=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[1]/div[1]/h1').text #Nosaukums
-    Vienotais_registracijas_numurs=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div[2]').text #Vienotais reģistrācijas numurs
-    SEPA_identifikators=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div[2]').text #SEPA identifikators
-    Registracijas_datums=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div[1]/div[1]/div[3]/div/div[2]').text #Reģistrācijas datums
-    Iesniegtie_gada_parskati=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div[1]/div[1]/div[6]/div/div/div[2]').text #Iesniegtie gada pārskati
-    Adrese=driver.find_element_by_xpath('/html/body/div[1]/div/div[1]/div[4]/div/div/div/div[2]/div/div[3]/div[2]/div/div[2]/div/div/div[1]/div[1]/div[4]/div/div[2]').text #Adrese
-    result_file.write(Url.rstrip()+"|"+\
-        Nosaukums+"|"+\
-        Vienotais_registracijas_numurs+"|"+\
-        Adrese+"|"+\
-        SEPA_identifikators+"|"+\
-        Registracijas_datums+"|"+\
-        Iesniegtie_gada_parskati+ '\n')
-    time.sleep(5)
-driver.close()
-result_file.close()
+big_list=[]
+for reg in my_list:
+    driver.get('https://info.ur.gov.lv/?#/legal-entity/'+reg)
+    time.sleep(6)
+    Url=str('https://info.ur.gov.lv/?#/legal-entity/'+reg)
+    soup=BeautifulSoup(driver.page_source, 'lxml')
+    firma=soup.find("h1", {"class": "Headline1"}).text.strip()
+    mylist=['', '', '', '', '']
+    for elem in soup.findAll("div", {"class": "Dropdown"}):
+        if re.findall('Maksātnespējas process\([1-9]\)', elem.text):
+            mylist[0]=str(reg.rstrip())
+            mylist[1]=str(firma)
+            mylist[2]="NEDERĪGS"
+            mylist[3]="Maksātnespējas process"
+    for elem in soup.find_all("div", {"class": "ContentLight"}):
+        if elem.text.strip() == "Izslēgts no reģistra":
+            mylist[0]=str(reg.rstrip())
+            mylist[1]=str(firma)
+            mylist[2]=str("NEDERĪGS")
+            mylist[3]=str(elem.text.strip())
+        elif elem.text.strip() == "Reģistrācijas datums" and mylist[2] == "NEDERĪGS":
+            mylist[4]="Reģistrācijas datums: "+str(elem.next_element.next_element.text.strip())
+        elif elem.text.strip() == "Reģistrācijas datums" and not mylist[2] == "NEDERĪGS":
+            mylist[0]=str(reg.rstrip())
+            mylist[1]=str(firma)
+            mylist[2]=str("DERĪGS")
+            mylist[4]="Reģistrācijas datums: "+str(elem.next_element.next_element.text.strip())
+    for elem in soup.find_all("div", {"class": "ContentLight"}):
+        if elem.text.strip() == "Adrese" and not mylist[2] == "NEDERĪGS":
+            mylist[3]="Adrese: "+str(elem.next_element.next_element.text.strip())  
+    big_list.append(mylist)
+#write big_list to file
+with open(path+"result.csv",'w') as f:
+    for sublist in big_list:
+        line = "{}|{}|{}|{}|{}\n".format(sublist[0], sublist[1], sublist[2], sublist[3], sublist[4])
+        f.write(line)
+    
+driver.quit()
 my_list.close()
 
 end = time.time()
